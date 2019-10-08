ad0e1c15289e5132a4d557e569d9494f46beadbc
chenchaoyun <chenchaoyun@megvii.com>
2019-10-08 14:25:03 +0800

    修改登陆页

--- src/store/modules/setting.js
+++ src/store/modules/setting.js
@@ -4,26 +4,26 @@ export default {
     isMobile: false,
     theme: 'dark',
     layout: 'side',
-    systemName: 'Vue Antd Admin',
-    copyright: '2018 ICZER 工作室出品',
-    footerLinks: [
-      {link: 'https://pro.ant.design', name: 'Pro首页'},
-      {link: 'https://github.com/iczer/vue-antd-admin', icon: 'github'},
-      {link: 'https://ant.design', name: 'Ant Design'}
-    ],
+    systemName: '开发调试工具集',
+    copyright: '2019',
+    // footerLinks: [
+    //   {link: 'https://pro.ant.design', name: 'Pro首页'},
+    //   {link: 'https://github.com/iczer/vue-antd-admin', icon: 'github'},
+    //   {link: 'https://ant.design', name: 'Ant Design'}
+    // ],
     multipage: true
   },
   mutations: {
-    setDevice (state, isMobile) {
+    setDevice(state, isMobile) {
       state.isMobile = isMobile
     },
-    setTheme (state, theme) {
+    setTheme(state, theme) {
       state.theme = theme
     },
-    setLayout (state, layout) {
+    setLayout(state, layout) {
       state.layout = layout
     },
-    setMultipage (state, multipage) {
+    setMultipage(state, multipage) {
       state.multipage = multipage
     }
   }
